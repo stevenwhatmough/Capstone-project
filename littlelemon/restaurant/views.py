e556006eb8446cf6aba6cc73a3b19a07fadec1d4
Steven Whatmough <mrstevenwhatmough@gmail.com>
2026-04-19 16:59:05 +0900

    Initial Table Booking API functionality added

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_Classes = [permissions.IsAuthenticated]
